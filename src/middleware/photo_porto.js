08d4135a487431f8105dbc0e4fc51df05500829e
vickomaris <64400928+vickomaris@users.noreply.github.com>
2022-12-26 11:25:11 +0700

    multer deploy

--- src/middleware/photo_porto.js
+++ src/middleware/photo_porto.js
@@ -5,9 +5,9 @@ const path = require('path')
 // untuk management file
 const multerUpload = multer({
   storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, './public/photo_porto')
-    },
+    // destination: (req, file, cb) => {
+    //   cb(null, './public/photo_porto')
+    // },
     filename: (req, file, cb) => {
       const ext = path.extname(file.originalname)
       const filename = Date.now() + '' + ext
